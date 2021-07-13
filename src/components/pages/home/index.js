7a301c638522fdbed5a685c51ec602bee6f455bc
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2021-07-14 03:26:55 +0430

    home changes

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,17 +3,24 @@ import "./home.css";
 import Layout from "../../common/layout";
 import methodesData from "./methodes/peyment-methods.data";
 import Methode from "./methodes";
-import userimage from "../../../assets/userimage.png"
-import {ReactComponent as BellIcon} from '../../../assets/bell.svg';
-import {ReactComponent as EnvelopeIcon} from '../../../assets/envelope.svg';
-import {ReactComponent as CalenderIcon} from '../../../assets/calendar.svg';
+import userimage from "../../../assets/userimage.png";
+import { ReactComponent as BellIcon } from "../../../assets/bell.svg";
+import { ReactComponent as EnvelopeIcon } from "../../../assets/envelope.svg";
+import { ReactComponent as CalenderIcon } from "../../../assets/calendar.svg";
+import PersonIcon from '@material-ui/icons/Person';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import SecurityOutlinedIcon from '@material-ui/icons/SecurityOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import Badge from "@material-ui/core/Badge";
+
 const Home = () => {
   return (
     <Layout>
       <div className="mainContainer home">
         <div className="center-container">
           <div className="search-container">
-            <input type="text" placeholder="Search in Dashboard..." />
+            <span ><SearchOutlinedIcon/></span>
+            <input type="text" placeholder='Search in Dashboard...' />
           </div>
           <div className="payment-methodes">
             <h1>Payment Methodes</h1>
@@ -25,59 +32,88 @@ const Home = () => {
           </div>
           <div className="income-overview"></div>
           <div className="yourLink">
-          <div className="textContainer ">
-          <div className="caption">
-            <p>
-            Your Referral Link :  
-              <span>
-              
-              </span>
-            </p>
-            <p>
-            Your Code : 
-              <span>
-              
-              </span>
-            </p>
-          </div>
-          <div className="circleTitle">
-            <p>45</p>
-            <p>Invited Friends</p>
-          </div>
-        </div>
-     
+            <div className="textContainer ">
+              <div className="caption">
+                <p>
+                  Your Referral Link :<span></span>
+                </p>
+                <p>
+                  Your Code :<span></span>
+                </p>
+              </div>
+              <div className="circleTitle">
+                <p>45</p>
+                <p>Invited Friends</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="right-container">
           <div className="profile-container">
             <div className="top-area">
               <div className="image-container">
-                <img src={userimage}/>
+                <img src={userimage} />
               </div>
               <p>Amir Ebrahimi</p>
             </div>
             <div className="icon-bar">
-              <div className="item"><BellIcon/></div>
-              <div className="item"><EnvelopeIcon/></div>
-              <div className="item"><CalenderIcon/></div>
+              <Badge
+                badgeContent={2}
+                overlap="circular"
+                max="99"
+                color="secondary"
+              >
+                <div className="item">
+                  <BellIcon />
+                </div>
+              </Badge>
+              <Badge
+                badgeContent={8}
+                overlap="circular"
+                max="99"
+                color="secondary"
+              >
+                <div className="item">
+                  <EnvelopeIcon />
+                </div>
+              </Badge>
+
+              <div className="item">
+                <CalenderIcon />
+              </div>
             </div>
             <div className="buttom-area">
-              <div className="item"><p>Edit Profile</p><span>icon</span></div>
-              <div className="item"><p>Account Settings</p><span>icon</span></div>
-              <div className="item"><p>Security</p><span>icon</span></div>
+              <div className="item">
+                <p>Edit Profile</p>
+                <span><PersonIcon/></span>
+              </div>
+              <div className="item">
+                <p>Account Settings</p>
+                <span><SettingsOutlinedIcon/></span>
+              </div>
+              <div className="item">
+                <p>Security</p>
+                <span><SecurityOutlinedIcon/></span>
+              </div>
             </div>
           </div>
           <div className="other-info-container">
             <div className="info-item">
-              <div className="circle">57,421</div>
+              <div className="circle"></div>
+              <div className="circle2"><h5>$</h5></div>
               <p>COMISSION WALLET</p>
+              <h5>57,421</h5>
             </div>
             <div className="info-item">
-              <div className="circle">125.65</div>
+              <div className="circle"></div>
+              <div className="circle2"><h5>$</h5></div>
               <p>TOTAL INVESTMENT</p>
+              <h5>125.65</h5>
             </div>
             <div className="info-item">
-              <div className="circle">87.89</div>
+              <div className="circle"></div>
+              <div className="circle2"><h5>$</h5></div>
+              <h5>87.89</h5>
               <p>PROFIT WALLET</p>
             </div>
           </div>
--- src/components/pages/home/methodes/index.js
+++ src/components/pages/home/methodes/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./methodes.css";
 import styled from "styled-components";
+import Logo from "../../../../assets/aboutus.png"
 
 const MethodeContainer = styled.div`
   width: fit-content;
@@ -11,11 +12,12 @@ const MethodeContainer = styled.div`
   flex-direction: column;
 `;
 const MethodeBox = styled.div`
-  border-radius: 20px;
-  -webkit-border-radius: 20px;
-  -moz-border-radius: 20px;
-  -ms-border-radius: 20px;
-  -o-border-radius: 20px;
+position:relative;
+  border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  -ms-border-radius: 10px;
+  -o-border-radius: 10px;
   color: white;
   padding: 5px;
   width: 90px;
@@ -63,14 +65,117 @@ margin-top: 7px;
     padding: 5px 10px;
     font-weight: 600;
 `;
+const Image = styled.div`
+position:absolute;
+top:1px;
+left:1px;
+width:58px;
+height:58px;
+background-image: url(${Logo});
+background-position:center;
+background-size:cover;
+background-repeat: no-repeat;
+`
+const Shadow = styled.div`
+position:absolute;
+top: 7px;
+    left: 6px;
+    width: 52px;
+    height: 52px;
+border-radius: 50%;
+-webkit-border-radius: 50%;
+-moz-border-radius: 50%;
+-ms-border-radius: 50%;
+-o-border-radius: 50%;
+bacground-color:transparent;
+box-shadow: 0 0 20px 1px white;
 
+`
+const Price = styled.p`
+position:absolute;
+border-radius: 6px;
+background-color: #b0bcf9;
+top:49px;
+right:-3px;
+padding:1px 7px;
+font-size:11px;
+font-weight:600;
+letter-spacing: 2px;
+`
+
+const Package = styled.p`
+position:absolute;
+top:47px;
+right:11px;
+font-size:5px;
+text-transform: uppercase;
+opacity: 0.8;
+`
+const Title = styled.p`
+position:absolute;
+top:15px;
+right:2px;
+margin:0;
+font-size:7px;
+text-transform: uppercase;
+  text-orientation: sideways;
+  -webkit-text-orientation: sideways;
+  writing-mode: vertical-rl;
+  -webkit-writing-mode:;
+  -ms-writing-mode:;
+  color: #f8f8f8;
+  font-weight: 900;
+  opacity: 0.8;
+  letter-spacing: 2px;
+  transform: rotate(180deg);
+  -webkit-transform: rotate(180deg);
+  -moz-transform: rotate(180deg);
+  -ms-transform: rotate(180deg);
+  -o-transform: rotate(180deg);
+
+`
+const Line1 = styled.div`
+width:25px;
+height:1px;
+background-color:#f8f8f8;
+position:absolute;
+top:65px;
+left:5px;
+opacity:0.5;
+`
+const Line2 = styled.div`
+width:15px;
+height:1px;
+background-color:#f8f8f8;
+position:absolute;
+top:73px;
+left:5px;
+opacity:0.5;
+`
+const Line3 = styled.div`
+width:10px;
+height:1px;
+background-color:#f8f8f8;
+position:absolute;
+top:81px;
+left:5px;
+opacity:0.5;
+`
 const index = ({ type, price }) => {
   console.log(type);
   return (
     <MethodeContainer>
       {
         <MethodeBox {...type}>
-          <p>{price}</p>
+           <Package>Package</Package>
+           <Title>CYRUS</Title>
+          <Price>{price}</Price>
+          <Shadow></Shadow>
+          <Line1></Line1>
+          <Line2></Line2>
+          <Line3></Line3>
+         
+          <Image/>
         </MethodeBox>
       }
 

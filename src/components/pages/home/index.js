96565e9c307da2153fe9873934b3ece994f07475
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2021-07-15 16:20:02 +0430

    profile edited

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -7,20 +7,24 @@ import userimage from "../../../assets/userimage.png";
 import { ReactComponent as BellIcon } from "../../../assets/bell.svg";
 import { ReactComponent as EnvelopeIcon } from "../../../assets/envelope.svg";
 import { ReactComponent as CalenderIcon } from "../../../assets/calendar.svg";
-import PersonIcon from '@material-ui/icons/Person';
-import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
-import SecurityOutlinedIcon from '@material-ui/icons/SecurityOutlined';
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import PersonIcon from "@material-ui/icons/Person";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+import SecurityOutlinedIcon from "@material-ui/icons/SecurityOutlined";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import HttpsIcon from "@material-ui/icons/Https";
 import Badge from "@material-ui/core/Badge";
 
 const Home = () => {
+  const lock = <HttpsIcon />;
   return (
     <Layout>
       <div className="mainContainer home">
         <div className="center-container">
           <div className="search-container">
-            <span ><SearchOutlinedIcon/></span>
-            <input type="text" placeholder='Search in Dashboard...' />
+            <span>
+              <SearchOutlinedIcon />
+            </span>
+            <input type="text" placeholder="Search in Dashboard..." />
           </div>
           <div className="payment-methodes">
             <h1>Payment Methodes</h1>
@@ -50,12 +54,14 @@ const Home = () => {
         </div>
         <div className="right-container">
           <div className="profile-container">
+            {/* top area == avatar , name */}
             <div className="top-area">
               <div className="image-container">
                 <img src={userimage} />
               </div>
               <p>Amir Ebrahimi</p>
             </div>
+            {/* icon bar */}
             <div className="icon-bar">
               <Badge
                 badgeContent={2}
@@ -82,37 +88,117 @@ const Home = () => {
                 <CalenderIcon />
               </div>
             </div>
+            {/* switch bar */}
+            <div className="switch-bar">
+              <p className="item active">Profile</p>
+              <p className="item disabeld">
+                History<span className="lock">{lock}</span>
+              </p>
+            </div>
+            {/* buttom area == edit , setting ,ب security  */}
             <div className="buttom-area">
+              {/* Edit Profile */}
               <div className="item">
                 <p>Edit Profile</p>
-                <span><PersonIcon/></span>
+                <span>
+                  <PersonIcon />
+                </span>
               </div>
+              <div id="edit-profile" className="edit-profile">
+                <div className="item">
+                  <p>Change NickName</p>
+                </div>
+                <div className="item">
+                  <p>Change Profile Picture</p>
+                </div>
+                <div className="item action">
+                  <div className="button-container">
+                    <button className="reset">reset</button>
+                    <button className="save">save</button>
+                  </div>
+                </div>
+              </div>
+              {/* Account Settings */}
               <div className="item">
                 <p>Account Settings</p>
-                <span><SettingsOutlinedIcon/></span>
+                <span>
+                  <SettingsOutlinedIcon />
+                </span>
               </div>
+              <div id="account-setting" className="account-setting">
+                <div className="item">
+                  <p>Change Password</p>
+                  <div className="input "  >
+<input type="password" id="pass" value="7656745343234" />
+                  </div>
+                </div>
+                <div className="item">
+                  <p>Change Email</p>
+                  <div className="input" id="email">
+<input type="email"  value="faeze.moradi8993@gmail.com" />
+                  </div>
+                </div>
+                <div className="item">
+                  <p>Change Phone Number</p>
+                  <div className="input">
+<input type="number" value="09123456789" />
+                  </div>
+                </div>
+                <div className="item action">
+                  <div className="button-container">
+                    <button className="reset">reset</button>
+                    <button className="save">save</button>
+                  </div>
+                </div>
+              </div>
+              {/* Security */}
               <div className="item">
                 <p>Security</p>
-                <span><SecurityOutlinedIcon/></span>
+                <span>
+                  <SecurityOutlinedIcon />
+                </span>
+              </div>
+              <div id="security" className="security">
+                <div className="item">
+                  <p>Authentication</p>
+                </div>
+                <div className="item">
+                  <p>Two-Facktor Authentication</p>
+                </div>
+                <div className="item">
+                  <p>Sms Backup Authentication</p>
+                </div>
+                <div className="item action">
+                  <div className="button-container">
+                    <button className="reset">reset</button>
+                    <button className="save">save</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="other-info-container">
             <div className="info-item">
               <div className="circle"></div>
-              <div className="circle2"><h5>$</h5></div>
+              <div className="circle2">
+                <h5>$</h5>
+              </div>
               <p>COMISSION WALLET</p>
               <h5>57,421</h5>
             </div>
             <div className="info-item">
               <div className="circle"></div>
-              <div className="circle2"><h5>$</h5></div>
+              <div className="circle2">
+                <h5>$</h5>
+              </div>
               <p>TOTAL INVESTMENT</p>
               <h5>125.65</h5>
             </div>
             <div className="info-item">
               <div className="circle"></div>
-              <div className="circle2"><h5>$</h5></div>
+              <div className="circle2">
+                <h5>$</h5>
+              </div>
               <h5>87.89</h5>
               <p>PROFIT WALLET</p>
             </div>
